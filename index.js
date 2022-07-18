843eb540ad61bc52d51e555eb23b7c1221df0888
inakiesk <inakieskruiz@gmail.com>
2022-07-18 17:57:32 -0500

    Correct html and js structure

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ let closeX = document.getElementById('closeX');
 let toolbar = document.getElementById('toolbar');
 let hamburger = document.getElementById('hamburger');
 
-console.log(toolbar);
-
-// hamburger.addEventListener('click', function () {
-//   closeX.classList.toggle('hide');
-//   toolbar.classList.toggle('hide');
-//   hamburger.classList.toggle('hide');
-// });
-
 [closeX, toolbar, hamburger].forEach((elements) => {
   elements.addEventListener('click', function () {
     closeX.classList.toggle('hide');
