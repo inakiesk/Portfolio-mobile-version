11810995dde136d4b5af6295255cba7996d2fe62
inakiesk <inakieskruiz@gmail.com>
2022-07-18 16:30:06 -0500

    Add js file with basic structure

---
+++ index.js
@@ -0,0 +1,6 @@
+let close = document.querySelector("#close");
+let toolbar = document.querySelector("#toolbar");
+let link1 = document.querySelector("link1");
+let link2 = document.querySelector("link2");
+let link3 = document.querySelector("link3");
+
