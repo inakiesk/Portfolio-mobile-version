0333f604e798316a6ba773048b4793d22f88fbe0
inakiesk <inakieskruiz@gmail.com>
2022-07-18 21:46:48 -0500

    Fix error that cause menu to close by clicking the background

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const closeX = document.getElementById('closeX');
 const toolbar = document.getElementById('toolbar');
 const hamburger = document.getElementById('hamburger');
+const menuBackground = document.getElementById('menuBackground');
 
 [closeX, toolbar, hamburger].forEach((elements) => {
   elements.addEventListener('click', () => {
     closeX.classList.toggle('hide');
     toolbar.classList.toggle('hide');
     hamburger.classList.toggle('hide');
+    menuBackground.classList.toggle('hide');
   });
 });
