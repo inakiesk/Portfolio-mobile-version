62c52aa989f5a0dcdc5e838bdf01231ad5dbae2d
Omachi John <omachi.john@.com>
2022-07-21 00:41:53 +0100

    Modify function parameters to avoid code repetition

--- index.js
+++ index.js
@@ -16,62 +16,62 @@ let popUp = {
   workHeader: '',
   subHeader: '',
   image: '',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   technologies: '',
   linkLive: 'See live',
-  linkSource: 'See source'
-}
+  linkSource: 'See source',
+};
 
 popUp.workHeader = document.getElementsByClassName('work-header');
 popUp.subHeader = document.getElementsByClassName('work-subheader');
 popUp.image = document.getElementsByClassName('workImg');
-popUp.technologies = document.getElementsByClassName('program-list')
-
+popUp.technologies = document.getElementsByClassName('program-list');
 
 const buttonWork1 = document.querySelector('.button-work1');
 
 buttonWork1.addEventListener('click', newPopUp);
 
-function newPopUp(){
-  const backPop = document.createElement("div");
+function newPopUp(a, b, c, d, e) {
+  const backPop = document.createElement('div');
   backPop.classList.add('popbackground');
-  const popCard = document.createElement("div");
+  const popCard = document.createElement('div');
   popCard.classList.add('popup-card');
-  const headerBox = document.createElement("div");
+  const headerBox = document.createElement('div');
   headerBox.classList.add('header-box');
-  const clonedHeader = popUp.workHeader[0].cloneNode(true);
+  const clonedHeader = popUp.workHeader[a].cloneNode(true);
   clonedHeader.classList.add('cloned-header');
   const Bclose = document.createElement('img');
   Bclose.src = 'images/bclose.png';
   Bclose.classList.add('b-close');
-  const clonedSubheader = popUp.subHeader[0].cloneNode(true);
+  const clonedSubheader = popUp.subHeader[b].cloneNode(true);
   clonedSubheader.classList.add('cloned-subheader');
-  const clonedImage = popUp.image[0].cloneNode(true);
+  const clonedImage = popUp.image[c].cloneNode(true);
   clonedImage.classList.add('cloned-img');
-  const clonedDesktopImage = popUp.image[1].cloneNode(true);
+  const clonedDesktopImage = popUp.image[d].cloneNode(true);
   clonedDesktopImage.classList.add('cloned-desk-img');
-  const popInfoContainer = document.createElement("div");
-  const popBodyContainer = document.createElement("div");
+  const popInfoContainer = document.createElement('div');
+  const popBodyContainer = document.createElement('div');
   popBodyContainer.classList.add('pop-body-container');
   popInfoContainer.classList.add('pop-info-container');
   const description = document.createElement('p');
   description.innerHTML = popUp.description;
   description.classList.add('pop-description');
-  const clonedTech = popUp.technologies[0].cloneNode(true);
+  const clonedTech = popUp.technologies[e].cloneNode(true);
   clonedTech.setAttribute('id', 'cloned-tech');
   const githubTech = document.createElement('li');
-  githubTech.innerHTML = "Github";
+  githubTech.innerHTML = 'Github';
   githubTech.classList.add('desktop');
   const rubyTech = document.createElement('li');
-  rubyTech.innerHTML = "Ruby";
+  rubyTech.innerHTML = 'Ruby';
   rubyTech.classList.add('desktop');
   const bootTech = document.createElement('li');
-  bootTech.innerHTML = "Bootstrap";
+  bootTech.innerHTML = 'Bootstrap';
   bootTech.classList.add('desktop');
-  const buttonBox = document.createElement("div");
-  buttonBox.classList.add("pop-button-box");
-  const liveButton = document.createElement("button");
-  const sourceButton = document.createElement("button");
+  const buttonBox = document.createElement('div');
+  buttonBox.classList.add('pop-button-box');
+  const liveButton = document.createElement('button');
+  const sourceButton = document.createElement('button');
   liveButton.innerHTML = popUp.linkLive;
   sourceButton.innerHTML = popUp.linkSource;
   liveButton.classList.add('pop-button');
@@ -105,11 +105,10 @@ function newPopUp(){
 
   Bclose.addEventListener('click', closePopUp);
 
-  function closePopUp(){
+  function closePopUp() {
     backPop.remove();
   }
 }
-
 
 console.log(popUp.workHeader);
 console.log(popUp.subHeader);
