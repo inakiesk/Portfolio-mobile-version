cf04e20e0ba401389a117c981796e83aa52462ab
inakiesk <inakieskruiz@gmail.com>
2022-07-19 22:05:16 -0500

    Add popup window on first work section link

--- index.js
+++ index.js
@@ -11,3 +11,38 @@ const menuBackground = document.getElementById('menuBackground');
     menuBackground.classList.toggle('hide');
   });
 });
+
+let popUp = {
+  workHeader: '',
+  subHeader: '',
+  image: '',
+  description: '',
+  technologies: '',
+  linkLive: '',
+  linkSource: ''
+}
+
+popUp.workHeader = document.getElementsByClassName('work-header');
+popUp.subHeader = document.getElementsByClassName('work-subheader');
+popUp.image = document.getElementsByClassName('workImg');
+popUp.technologies = document.getElementsByClassName('program-list')
+
+
+const buttonWork1 = document.querySelector('.button-work1');
+
+buttonWork1.addEventListener('click', newPopUp);
+
+function newPopUp(){
+  const backPop = document.createElement("div");
+  const popCard = document.createElement("div");
+  backPop.appendChild(popCard);
+  popCard.appendChild(popUp.workHeader[0]);
+  backPop.classList.add('popbackground');
+  popCard.classList.add('popup-card');
+  document.body.appendChild(backPop);
+}
+
+console.log(popUp.workHeader);
+console.log(popUp.subHeader);
+console.log(popUp.image);
+console.log(popUp.technologies);
