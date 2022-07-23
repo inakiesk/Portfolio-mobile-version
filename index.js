503ca2af7595f721abf160205ae7507197e57b75
inakiesk <inakieskruiz@gmail.com>
2022-07-22 19:29:22 -0500

    Complete and organize function structure for correct display

--- index.js
+++ index.js
@@ -13,158 +13,65 @@ const menuBackground = document.getElementById('menuBackground');
 });
 
 const arrProjects = [
-    {
-      id: 0,
-      header: 'Tonic',
-      desktopHeader: 'Tonic',
-      subHeader: ['CANOPY', 'Back End Dev', '2015'],
-      deskSubHeader: ['CANOPY', 'Back End Dev', '2015'],
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-      img: "./images/work1-img.svg",
-      desktopImg: "images/work1-desktop-img.svg",
-      technologies: ['html', 'css', 'javascript'],
-      desktopTech: ['Github', 'Ruby', 'Bootstrap'],
-      linkLive: 'See live',
-      linkSource: 'See source',
-    },
-    {
-      id: 1,
-      header: 'Multi-Post Stories',
-      desktopHeader: 'Multi-Post Stories',
-      subHeader: ['CANOPY', 'Back End Dev', '2015'],
-      deskSubHeader: ['FACEBOOK', 'FULL STACK DEV', '2015'],
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-      img: "images/work2-img.svg",
-      desktopImg: "./images/work4-img.svg",
-      technologies: ['html', 'css', 'javascript'],
-      desktopTech: ['Github', 'Ruby', 'Bootstrap'],
-      linkLive: 'See live',
-      linkSource: 'See source',
-    },
-    {
-      id: 2,
-      header: 'Tonic',
-      desktopHeader: 'Facebook 360',
-      subHeader: ['CANOPY', 'Back End Dev', '2015'],
-      deskSubHeader: ['FACEBOOK', 'FULL STACK DEV', '2015'],
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-      img: "images/work3-img.svg",
-      desktopImg: "./images/work1-img.svg",
-      technologies: ['html', 'css', 'javascript'],
-      desktopTech: ['Github', 'Ruby', 'Bootstrap'],
-      linkLive: 'See live',
-      linkSource: 'See source',
-    },
-    {
-      id: 3,
-      header: 'Multi-Post Stories',
-      desktopHeader: 'Uber Navigation',
-      subHeader: ['CANOPY', 'Back End Dev', '2015'],
-      deskSubHeader: ['Uber', 'Lead Developer', '2018'],
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-      img: "images/work4-img.svg",
-      desktopImg: "./images/work2-img.svg",
-      technologies: ['html', 'css', 'javascript'],
-      desktopTech: ['Github', 'Ruby', 'Bootstrap'],
-      linkLive: 'See live',
-      linkSource: 'See source',
-    },
-]
-
+  {
+    header: 'Tonic',
+    desktopHeader: 'Tonic',
+    subHeader: ['CANOPY', 'Back End Dev', '2015'],
+    deskSubHeader: ['CANOPY', 'Back End Dev', '2015'],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    img: 'images/work1-img.svg',
+    desktopImg: 'images/work1-desktop-img.svg',
+    technologies: ['html', 'css', 'javascript'],
+    desktopTech: ['Github', 'Ruby', 'Bootstrap'],
+    linkLive: 'https://inakiesk.github.io/Portfolio-mobile-version/',
+    linkSource: 'https://github.com/inakiesk/Portfolio-mobile-version',
+  },
+  {
+    header: 'Multi-Post Stories',
+    desktopHeader: 'Multi-Post Stories',
+    subHeader: ['CANOPY', 'Back End Dev', '2015'],
+    deskSubHeader: ['FACEBOOK', 'FULL STACK DEV', '2015'],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    img: 'images/work2-img.svg',
+    desktopImg: 'images/work4-img.svg',
+    technologies: ['html', 'css', 'javascript'],
+    desktopTech: ['Github', 'Ruby', 'Bootstrap'],
+    linkLive: 'https://inakiesk.github.io/Portfolio-mobile-version/',
+    linkSource: 'https://github.com/inakiesk/Portfolio-mobile-version',
+  },
+  {
+    header: 'Tonic',
+    desktopHeader: 'Facebook 360',
+    subHeader: ['CANOPY', 'Back End Dev', '2015'],
+    deskSubHeader: ['FACEBOOK', 'FULL STACK DEV', '2015'],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    img: 'images/work3-img.svg',
+    desktopImg: 'images/work1-img.svg',
+    technologies: ['html', 'css', 'javascript'],
+    desktopTech: ['Github', 'Ruby', 'Bootstrap'],
+    linkLive: 'https://inakiesk.github.io/Portfolio-mobile-version/',
+    linkSource: 'https://github.com/inakiesk/Portfolio-mobile-version',
+  },
+  {
+    header: 'Multi-Post Stories',
+    desktopHeader: 'Uber Navigation',
+    subHeader: ['CANOPY', 'Back End Dev', '2015'],
+    deskSubHeader: ['Uber', 'Lead Developer', '2018'],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    img: 'images/work4-img.svg',
+    desktopImg: 'images/work2-img.svg',
+    technologies: ['html', 'css', 'javascript'],
+    desktopTech: ['Github', 'Ruby', 'Bootstrap'],
+    linkLive: 'https://inakiesk.github.io/Portfolio-mobile-version/',
+    linkSource: 'https://github.com/inakiesk/Portfolio-mobile-version',
+  },
+];
 
 const buttonWork = document.querySelector('.button-work');
 
-function newPopUp(a) {
-  
-  /*const clonedHeader = arrProjects.workHeader[a].cloneNode(true);*/
-/*
-        -const clonedHeader = document.createElement('h2');
-        clonedHeader.innerHTML = arrProjects[a].header;
-        clonedHeader.classList.add('work-header');
-        clonedHeader.classList.add('mobile'); 
-*/
-
-  /*const clonedHeader2 = popUp.workHeader[b].cloneNode(true);*/
-/*
-        -const clonedHeader2 = document.createElement('h2')
-        clonedHeader2.innerHTML = arrProjects[a].desktopHeader;
-        clonedHeader2.classList.add('work-header');
-        clonedHeader2.classList.add('desktop');
-*/
-/*
-  const clonedSubheader = popUp.subHeader[c].cloneNode(true);
-
-        -const clonedSubheader = document.createElement('ul');
-        clonedSubheader.classList.add('work-subheader');
-        const list1m = document.createElement('li');
-        const list1d = document.createElement('li');
-        const dot1 = document.createElement('li');
-        const list2m = document.createElement('li');
-        const list2d = document.createElement('li');
-        const dot2 = document.createElement('li');
-        const list3m = document.createElement('li');
-        const list3d = document.createElement('li');
-        list1m.innerHTML = arrProjects[a].subHeader[0];
-        list1d.innerHTML = arrProjects[a].deskSubHeader[0];
-        list2m.innerHTML = arrProjects[a].subHeader[1];
-        list2d.innerHTML = arrProjects[a].deskSubHeader[1];
-        list3m.innerHTML = arrProjects[a].subHeader[2];
-        list3d.innerHTML = arrProjects[a].deskSubHeader[2];
-        list1m.classList.add('list1', 'mobile');
-        list1d.classList.add('list1', 'desktop');
-        dot1.classList.add('dot');
-        list2m.classList.add('list2', 'mobile');
-        list2d.classList.add('list2', 'desktop');
-        dot2.classList.add('dot');
-        list3m.classList.add('list2', 'mobile' );
-        list3m.classList.add('list2', 'desktop');
-        clonedSubheader.appendChild(list1m);
-        clonedSubheader.appendChild(list1d);
-        clonedSubheader.appendChild(dot1);
-        clonedSubheader.appendChild(list2m);
-        clonedSubheader.appendChild(list2d);
-        clonedSubheader.appendChild(dot2);
-        clonedSubheader.appendChild(list3m);
-        clonedSubheader.appendChild(list3d);
-*/
-
-
-  /*const clonedImage = popUp.image[d].cloneNode(true);*/
-/*
-        const clonedImage = document.createElement('img');
-        clonedImage.src = arrProjects.img;
-        clonedImage.classList.add('mobile', 'workImg',)
-*/
-
- /* const clonedDesktopImage = popUp.image[e].cloneNode(true); */
-/*
-        const clonedDesktopImage = document.createElement('img');
-        clonedDesktopImage.src = arrProjects.desktopImg;
-        clonedDesktopImage.classList.add('desktop', 'workImg');
-*/
-  
-  
-  /*const clonedTech = popUp.technologies[f].cloneNode(true); */
- /* 
-        const clonedTech = document.createElement('ul');
-        clonedTech.classList.add('program-list');
-        clonedTech.setAttribute("id", "cloned-tech");
-        const tech1 = document.createElement('li');
-        const tech2 = document.createElement('li');
-        const tech3 = document.createElement('li');
-        tech1.innerHTML = arrProjects[a].technologies[0];
-        tech2.innerHTML = arrProjects[a].technologies[1];
-        tech3.innerHTML = arrProjects[a].technologies[2];
-  clonedTech.appendChild(tech1);
-  clonedTech.appendChild(tech2);
-  clonedTech.appendChild(tech3);
-*/
-
-
-/*--------new--------*/
-
+function newPopUp(a, b, c, d) {
   const clonedHeader = document.createElement('h2');
-  const clonedHeader2 = document.createElement('h2')
+  const clonedHeader2 = document.createElement('h2');
   const clonedSubheader = document.createElement('ul');
   const list1m = document.createElement('li');
   const list1d = document.createElement('li');
@@ -180,10 +87,6 @@ function newPopUp(a) {
   const tech1 = document.createElement('li');
   const tech2 = document.createElement('li');
   const tech3 = document.createElement('li');
-/*-------------------*/
-
-  
-  
   const backPop = document.createElement('div');
   const popCard = document.createElement('div');
   const headerBox = document.createElement('div');
@@ -200,44 +103,38 @@ function newPopUp(a) {
   const liveIcon = document.createElement('img');
   const gitIcon = document.createElement('img');
 
-
-/*-----------------new-----------------------*/
-
   clonedHeader.innerHTML = arrProjects[a].header;
   clonedHeader.classList.add('work-header');
-  clonedHeader.classList.add('mobile'); 
+  clonedHeader.classList.add('mobile');
   clonedHeader2.innerHTML = arrProjects[a].desktopHeader;
   clonedHeader2.classList.add('work-header');
   clonedHeader2.classList.add('desktop');
   clonedSubheader.classList.add('work-subheader');
-  list1m.innerHTML = arrProjects[a].subHeader[0];
-  list1d.innerHTML = arrProjects[a].deskSubHeader[0];
-  list2m.innerHTML = arrProjects[a].subHeader[1];
-  list2d.innerHTML = arrProjects[a].deskSubHeader[1];
-  list3m.innerHTML = arrProjects[a].subHeader[2];
-  list3d.innerHTML = arrProjects[a].deskSubHeader[2];
+  list1m.innerHTML = arrProjects[a].subHeader[b];
+  list1d.innerHTML = arrProjects[a].deskSubHeader[b];
+  list2m.innerHTML = arrProjects[a].subHeader[c];
+  list2d.innerHTML = arrProjects[a].deskSubHeader[c];
+  list3m.innerHTML = arrProjects[a].subHeader[d];
+  list3d.innerHTML = arrProjects[a].deskSubHeader[d];
   list1m.classList.add('list1', 'mobile');
   list1d.classList.add('list1', 'desktop');
   dot1.classList.add('dot');
   list2m.classList.add('list2', 'mobile');
   list2d.classList.add('list2', 'desktop');
   dot2.classList.add('dot');
-  list3m.classList.add('list2', 'mobile' );
+  list3m.classList.add('list2', 'mobile');
   list3m.classList.add('list2', 'desktop');
   clonedImage.src = arrProjects[a].img;
-  clonedImage.classList.add('mobile', 'workImg',)
+  clonedImage.classList.add('mobile', 'workImg');
   clonedDesktopImage.src = arrProjects[a].desktopImg;
   clonedDesktopImage.classList.add('desktop', 'workImg');
   clonedTech.classList.add('program-list');
-  clonedTech.setAttribute("id", "cloned-tech");
-  tech1.innerHTML = arrProjects[a].technologies[0];
-  tech2.innerHTML = arrProjects[a].technologies[1];
-  tech3.innerHTML = arrProjects[a].technologies[2];
-
-
-
-/*---------------------------------------*/
-
+  clonedTech.setAttribute('id', 'cloned-tech');
+  tech1.innerHTML = arrProjects[a].technologies[b];
+  tech2.innerHTML = arrProjects[a].technologies[c];
+  tech3.innerHTML = arrProjects[a].technologies[d];
+  liveButton.innerHTML = 'See live';
+  sourceButton.innerHTML = 'See source';
   backPop.classList.add('popbackground');
   popCard.classList.add('popup-card');
   headerBox.classList.add('header-box');
@@ -253,9 +150,9 @@ function newPopUp(a) {
   clonedDesktopImage.classList.add('cloned-desk-img');
   popBodyContainer.classList.add('pop-body-container');
   popInfoContainer.classList.add('pop-info-container');
-  description.innerHTML = arrProjects.description;
+  description.innerHTML = arrProjects[a].description;
   description.classList.add('pop-description');
-  description.innerHTML = arrProjects.description;
+  description.innerHTML = arrProjects[a].description;
   description.classList.add('pop-description');
   clonedTech.setAttribute('id', 'cloned-tech');
   githubTech.innerHTML = 'Github';
@@ -265,8 +162,6 @@ function newPopUp(a) {
   bootTech.innerHTML = 'Bootstrap';
   bootTech.classList.add('desktop');
   buttonBox.classList.add('pop-button-box');
-  liveButton.innerHTML = arrProjects.linkLive;
-  sourceButton.innerHTML = arrProjects.linkSource;
   liveButton.classList.add('pop-button');
   sourceButton.classList.add('pop-button');
   liveIcon.src = 'images/live.svg';
@@ -274,7 +169,6 @@ function newPopUp(a) {
   gitIcon.src = 'images/github-blue.svg';
   gitIcon.classList.add('pop-button-icon');
 
-/*--------------------------- appends ---------------*/
   clonedSubheader.appendChild(list1m);
   clonedSubheader.appendChild(list1d);
   clonedSubheader.appendChild(dot1);
@@ -286,12 +180,6 @@ function newPopUp(a) {
   clonedTech.appendChild(tech1);
   clonedTech.appendChild(tech2);
   clonedTech.appendChild(tech3);
-
-
-
-
-/*----------------------------------------------*/
-
   document.body.appendChild(backPop);
   backPop.appendChild(popCard);
   popCard.appendChild(headerBox);
@@ -317,14 +205,20 @@ function newPopUp(a) {
   function closePopUp() {
     backPop.remove();
   }
+
+  liveButton.addEventListener('click', () => {
+    window.open(arrProjects[a].linkLive);
+  });
+
+  sourceButton.addEventListener('click', () => {
+    window.open(arrProjects[a].linkSource);
+  });
+
   Bclose.addEventListener('click', closePopUp);
 }
 
 buttonWork.addEventListener('click', newPopUp);
 
-
-console.log(clonedImage);
-console.log(clonedDesktopImage);
 /* Form------------------- validation -------------------*/
 
 const email = document.getElementById('email');
